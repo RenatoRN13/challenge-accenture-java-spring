8d13c4832893483b0d1cf3c75e15a02e297b578f
Renato Filho <renatocrvg@ufrn.edu.br>
2020-10-17 15:42:03 -0300

    projeto inicial

---
+++ src/main/java/com/example/AccentureChallenge/AccentureChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AccentureChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AccentureChallenge/AccentureChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccentureChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
